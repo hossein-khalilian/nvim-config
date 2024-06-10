435ffb2b15e9e78b4a876d8da2130dc0d0509659
hossein <hse.khalilian08@gmail.com>
2024-06-10 13:31:14 +0330

    update bufferline config for multitabs

--- lua/hossein/plugins/bufferline.lua
+++ lua/hossein/plugins/bufferline.lua
@@ -1,10 +1,8 @@
 return {
   "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-    },
-  },
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("bufferline").setup({})
+  end,
 }
