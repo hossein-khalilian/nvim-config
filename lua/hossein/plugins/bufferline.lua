02771e4cb3609114614dd4029e2db4de7fc93c63
hossein <hse.khalilian08@gmail.com>
2024-06-10 16:00:16 +0330

    add key maps for bufferline

--- lua/hossein/plugins/bufferline.lua
+++ lua/hossein/plugins/bufferline.lua
@@ -4,5 +4,8 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     require("bufferline").setup({})
+    vim.api.nvim_set_keymap("n", "<A-,>", "<Cmd>bp<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<A-.>", "<Cmd>bn<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<A-c>", "<Cmd>bd<CR>", { noremap = true, silent = true })
   end,
 }
