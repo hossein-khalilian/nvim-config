497a6aa7c7adbe847864ffbb0a5bc2fdf5c9e678
hossein khalilian <hse.khalilian08@gmail.com>
2024-06-29 10:06:39 +0000

    update telescope config

--- lua/hossein/plugins/telescope.lua
+++ lua/hossein/plugins/telescope.lua
@@ -3,7 +3,15 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+
+    -- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
   },
