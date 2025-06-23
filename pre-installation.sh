56931881a2b3bb7322e1664ee651c627dec4fe76
hossein <hse.khalilian08@gmail.com>
2025-06-23 12:25:45 +0330

    add xclip

--- pre-installation.sh
+++ pre-installation.sh
@@ -5,7 +5,7 @@ set -e
 
 # Update package list and install dependencies
 sudo apt update
-sudo apt install -y unzip ripgrep build-essential python3-venv curl wget
+sudo apt install -y unzip ripgrep build-essential python3-venv curl wget xclip
 
 # Install Neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
