f4dd9a64ee241149272e59f91473dabe71f36dc3
hossein <hse.khalilian08@gmail.com>
2025-06-29 17:20:10 +0330

    add isort to mason

--- lua/hossein/plugins/lsp/mason.lua
+++ lua/hossein/plugins/lsp/mason.lua
@@ -44,7 +44,7 @@ return {
       ensure_installed = {
         "prettier", -- prettier formatter
         -- "stylua", -- lua formatter
-        -- "isort", -- python formatter
+        "isort", -- python formatter
         "black", -- python formatter
         -- "eslint_d",
         -- "jedi-language-server",
