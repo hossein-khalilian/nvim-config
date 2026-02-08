c2f9b457053cd6df9ed5194966b94410c1f67bd5
hossein khalilian <hse.khalilian08@gmail.com>
2026-02-08 12:11:30 +0330

    remove markdown preview

--- lua/hossein/plugins/markdown-preview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  ft = "markdown",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install", -- or: "cd app && npm install"
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-}
